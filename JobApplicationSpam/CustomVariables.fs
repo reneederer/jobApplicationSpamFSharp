4d5e69c0d9f20907904d9f2c861b38cc75c2f83c
reneederer <reneederer@arcor.de>
2018-01-19 18:30:11 +0100

    Implement custom variables

--- JobApplicationSpam/CustomVariables.fs
+++ JobApplicationSpam/CustomVariables.fs
@@ -2,14 +2,44 @@
 module CustomVariables =
     open FParsec
 
-    type Token =
-    | Variable of string
-    | EqualsSign
-    | Constant of string
-    | Match
+    (*
+        match $chefGeschlecht with
+        | "m" -> "Sehr geehrter Herr $chefTitel $chefNachname,"
+        | "f" -> "Sehr geehrte Frau $chefTitel $chefNachname,"
+        | "u" -> "Sehr geehrte Damen und Herren,"
+    *)
+
+    type VariableName = string
+    type VariableValue = string
+
+    type Variable =
+    | PureString of string * string
 
     type Expression =
-    | MatchExpression of Map<string, string>
+    | MatchExpression of VariableName * list<Variable>
+    | VariableExpression of Variable
+
+    let tryGetValue (expression : Expression) =
+        match expression with
+        | MatchExpression (variableName, variables) ->
+                variables
+                |> List.tryPick (fun variable ->
+                    match variable with
+                    | PureString (k, v) -> if k = variableName then Some v else None
+                )
+        | VariableExpression variable ->
+            match variable with
+            | PureString (k, v) ->
+                Some v
+    
+
+
+    let expr1 = VariableExpression (PureString ("$meinVorname", "René"))
+    let expr =
+        MatchExpression ("$chefVorname", [PureString ("$chefName", "Meier")])
+
+
+
 
     let tokenize str =
         ()
@@ -21,3 +51,10 @@ module CustomVariables =
 
 
 
+    let test p str =
+        match run p str with
+        | Success(result, _, _)   -> printfn "Success: %A" result
+        | Failure(errorMsg, _, _) -> printfn "Failure: %s" errorMsg
+   
+
+    
